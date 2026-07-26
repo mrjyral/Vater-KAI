6b99b8505058c458519ef3aa0bdb9251f53d63b4
mrjyral <mrjyral@gmail.com>
2026-07-27 00:29:07 +0200

    Create instagram.ts

---
+++ lib/instagram.ts
@@ -0,0 +1,15 @@
+const GRAPH_URL = 'https://graph.facebook.com/v19.0'
+export function buildAuthUrl() {
+  const redirect = `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/instagram/callback`
+  const scopes = 'instagram_basic,instagram_manage_messages,pages_show_list,pages_messaging'
+  return `https://www.facebook.com/v19.0/dialog/oauth?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${encodeURIComponent(redirect)}&scope=${scopes}&response_type=code`
+}
+export async function sendInstagramDM(recipientId: string, message: string, accessToken: string) {
+  const igUserId = process.env.INSTAGRAM_BUSINESS_ID!
+  const res = await fetch(`${GRAPH_URL}/${igUserId}/messages`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ recipient: { id: recipientId }, message: { text: message }, access_token: accessToken })
+  })
+  return res.json()
+}
