74dde2c8c649bdc8d9f4ee7cb955d8505cdff14a
mrjyral <mrjyral@gmail.com>
2026-07-27 12:54:08 +0200

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,2 +1,2 @@
 import { createClient } from '@supabase/supabase-js'
-export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://placeholder.supabase.co', process.env.NEXT_PUBLIC_SUPABASE_AN
+export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://gwhlgjxkbgfqrgobqyvm.supabase.co/rest/v1/', process.env.NEXT_PUBLIC_SUPABASE_AN
