823754ce7d5fbcfb9a71e9e03aaeb1ecfe3dda26
mrjyral <mrjyral@gmail.com>
2026-07-29 21:36:07 +0200

    Create page.tsx

---
+++ app/data-deletion/page.tsx
@@ -0,0 +1,11 @@
+export default function Privacy() {
+  return (
+    <div style={{padding: 40, fontFamily: 'sans-serif'}}>
+      <h1>Datenschutzrichtlinie</h1>
+      <p>Diese App dient zur Verwaltung von Instagram Nachrichten.</p>
+      <p>Wir speichern keine persönlichen Daten. Token werden sicher verarbeitet.</p>
+      <p>Kontakt: andreaswiede1982@gmail.com</p>
+      <p>Stand: 29.07.2026</p>
+    </div>
+  )
+}
